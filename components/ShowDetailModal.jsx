17244ea1bcf158ab98a62159230951edc1b42ba2
selman <gunduzgece546@gmail.com>
2023-01-21 15:06:51 +0300

    deatail

--- components/ShowDetailModal.jsx
+++ components/ShowDetailModal.jsx
@@ -32,17 +32,17 @@ const ShowDetailModal = ({detailData, showDetail,setShowDetail,setShowSell,setSh
                 <p className="text-center text-red font-bold md:text-2xl text-xl ">
                   {detailData?.name}
                 </p>
-                <p className="text-lg leading-[30px] text-center md:text-left">{detailData?.position}</p>
+                <p className="text-lg leading-[30px] md:ml-0 ml-6 text-center md:text-left">{detailData?.position}</p>
               </div>
-              <div className="p-6  bg-market-grey md:w-[421px] w-[90%] mx-auto md:mx-0 rounded-lg md:h-24">
+              <div className="md:p-6 p-2 md:mt-0 mt-4  bg-market-grey md:w-[421px] w-[full] mx-auto md:mx-0 rounded-lg md:h-24">
                 <div className="flex items-center justify-between">
-                  <p className="font-bold text-2xl w-[171px]">
+                  <p className="font-bold md:text-2xl w-[171px]">
                     {' '}
                     € {detailData?.price}.00
                   </p>
                   {showDetail.type == 'Buy' ? (
                     <button
-                      className=" bg-red w-[174px] text-white tex-2xl font-bold  rounded-lg h-12"
+                      className=" bg-red md:w-[174px] w-28 text-white md:tex-2xl font-bold  rounded-lg md:h-12 h-8"
                       onClick={() =>
                         setShowBuy({
                           show: true,
@@ -56,7 +56,7 @@ const ShowDetailModal = ({detailData, showDetail,setShowDetail,setShowSell,setSh
                     </button>
                   ) : (
                     <button
-                      className=" bg-red w-[174px] text-white tex-2xl font-bold  rounded-lg h-12"
+                      className=" bg-red md:w-[174px] w-28 text-white md:tex-2xl font-bold  rounded-lg md:h-12 h-8"
                       onClick={() =>
                         setShowSell({
                           show: true,
@@ -71,71 +71,71 @@ const ShowDetailModal = ({detailData, showDetail,setShowDetail,setShowSell,setSh
                 </div>
               </div>
             </div>
-            <div className="md:p-6 p-2 bg-market-grey my-6 md:mx-0 mx-2 rounded-lg">
+            <div className="md:p-6 p-2 bg-market-grey md:my-6 my-6 md:mx-0  rounded-lg">
               <p className="font-bold leading-[30px] text-lg ">ATTRIBUTES</p>
-              <div className="flex mt-6 gap-[22px] md:flex-nowrap flex-wrap">
-                <div className="md:p-6 p-4 bg-white rounded-lg md:w-[118px] w-28 md:h-24 h-18">
+              <div className="flex md:mt-6 md:gap-[22px] gap-2  md:flex-nowrap flex-wrap ">
+                <div className="md:p-6 px-2  bg-white rounded-lg md:w-[118px] w-[calc(50%-8px)] md:block flex md:h-24 md:h-18 ">
                   <p className="text-base md:leading-[30px] leading-8">Pace</p>
                   <p>
-                    <span className="font-bold md:text-2xl text-lg">
+                    <span className="font-bold md:text-2xl md:ml-0 ml-2">
                       {detailData?.attributes?.pace}
                     </span>
-                    <span className="text-attribute md:text-[20px] text-lg">/100</span>
+                    <span className="text-attribute md:text-[20px] leading-8">/100</span>
                   </p>
                 </div>
-                <div className="md:p-6 p-4 bg-white rounded-lg md:w-[118px] w-28 md:h-24 h-18 ">
+                <div className="md:p-6 px-2  bg-white rounded-lg md:w-[118px] w-[calc(50%-8px)] md:block flex md:h-24 md:h-18 ">
                   <p className="text-base md:leading-[30px] leading-8">Shooting</p>
                   <p>
-                    <span className="font-bold md:text-2xl text-lg">
+                    <span className="font-bold md:text-2xl md:ml-0 ml-2">
                       {detailData?.attributes?.shooting}
                     </span>
-                    <span className="text-attribute md:text-[20px] text-lg">/100</span>
+                    <span className="text-attribute md:text-[20px] leading-8">/100</span>
                   </p>
                 </div>
-                <div className="md:p-6 p-4 bg-white rounded-lg md:w-[118px] w-28 md:h-24 h-18">
+                <div className="md:p-6 px-2  bg-white rounded-lg md:w-[118px] w-[calc(50%-8px)] md:block flex md:h-24 md:h-18">
                   <p className="text-base md:leading-[30px] leading-8">Passing</p>
                   <p>
-                    <span className="font-bold md:text-2xl text-lg">
+                    <span className="font-bold md:text-2xl md:ml-0 ml-2">
                       {detailData?.attributes?.passing}
                     </span>
-                    <span className="text-attribute md:text-[20px] text-lg">/100</span>
+                    <span className="text-attribute md:text-[20px] leading-8">/100</span>
                   </p>
                 </div>
-                <div className="md:p-6 p-4 bg-white rounded-lg md:w-[118px] w-28 md:h-24 h-18 ">
+                <div className="md:p-6 px-2  bg-white rounded-lg md:w-[118px] w-[calc(50%-8px)] md:block flex md:h-24 md:h-18">
                   <p className="text-base md:leading-[30px] leading-8">Dribbling</p>
                   <p>
-                    <span className="font-bold md:text-2xl text-lg">
+                    <span className="font-bold md:text-2xl md:ml-0 ml-2">
                       {detailData?.attributes?.dribbling}
                     </span>
-                    <span className="text-attribute md:text-[20px] text-lg">/100</span>
+                    <span className="text-attribute md:text-[20px] leading-8">/100</span>
                   </p>
                 </div>
-                <div className="md:p-6 p-4 bg-white rounded-lg md:w-[118px] w-28 md:h-24 h-18 ">
+                <div className="md:p-6 px-2  bg-white rounded-lg md:w-[118px] w-[calc(50%-8px)] md:block flex md:h-24 md:h-18">
                   <p className="text-base md:leading-[30px] leading-8">Defending</p>
                   <p>
-                    <span className="font-bold md:text-2xl text-lg">
+                    <span className="font-bold md:text-2xl md:ml-0 ml-2">
                       {detailData?.attributes?.defending}
                     </span>
-                    <span className="text-attribute md:text-[20px] text-lg">/100</span>
+                    <span className="text-attribute md:text-[20px] leading-8">/100</span>
                   </p>
                 </div>
-                <div className="md:p-6 p-4 bg-white rounded-lg md:w-[118px] w-28 md:h-24 h-18 ">
+                <div className="md:p-6 px-2  bg-white rounded-lg md:w-[118px] w-[calc(50%-8px)] md:block flex md:h-24 md:h-18">
                   <p className="text-base md:leading-[30px] leading-8">Physical</p>
                   <p>
-                    <span className="font-bold md:text-2xl text-lg">
+                    <span className="font-bold md:text-2xl md:ml-0 ml-2">
                       {detailData?.attributes?.physical}
                     </span>
-                    <span className="text-attribute md:text-[20px] text-lg">/100</span>
+                    <span className="text-attribute md:text-[20px] leading-8">/100</span>
                   </p>
                 </div>
               </div>
             </div>
             <div className="md:flex md:gap-6 ">
-              <div className="bg-market-grey md:p-6 p-4 md:text-left text-center md:mb-0 mb-6 md:w-[421px] md:h-24 rounded-lg ">
+              <div className="bg-market-grey md:p-6 md:block flex p-2 justify-between md:text-left text-center md:mb-0 mb-6 md:w-[421px] md:h-24 rounded-lg ">
                 <p className="text-lg leading-[30px]">Team</p>
                 <p className="text-2xl font-bold">{detailData?.team}</p>
               </div>
-              <div className="bg-market-grey md:p-6 p-4 md:text-left text-center md:mb-0 mb-6 md:w-[421px] md:h-24 rounded-lg ">
+              <div className="bg-market-grey md:p-6 md:block flex p-2 justify-between md:text-left text-center md:mb-0 mb-6 md:w-[421px] md:h-24 rounded-lg ">
                 <p className="text-lg leading-[30px]">Card Type</p>
                 <p className="text-2xl font-bold">{detailData?.cardType}</p>
               </div>
